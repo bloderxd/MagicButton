6129d9a621e25002c6b165ff2f602d65bbccebee
Daniel <wowmegadenis@hotmail.com>
2016-07-28 00:37:07 -0300

    Add logic to magic support class

--- magic/src/main/java/br/com/bloder/magic/Magic.java
+++ magic/src/main/java/br/com/bloder/magic/Magic.java
@@ -1,7 +1,24 @@
 package br.com.bloder.magic;
 
+import android.view.View;
+
+import br.com.bloder.magic.internal.Back;
+import br.com.bloder.magic.internal.Go;
+
 /**
  * Created by bloder on 28/07/16.
  */
 public class Magic {
+
+  public void doWith(View button) {
+    if(button.getWidth() == button.getHeight()) {
+      Go animator = new Go(button);
+      animator.setDuration(200);
+      button.startAnimation(animator);
+    } else {
+      Back animator = new Back(button);
+      animator.setDuration(200);
+      button.startAnimation(animator);
+    }
+  }
 }
